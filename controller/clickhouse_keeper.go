ab9ae1ff520a7e9ac47bd71cb2a3bf4c41d8d634
Miran <miran@248.sh>
2025-08-04 19:37:06 +0200

    chore: switch the order of :: and 0.0.0.0

--- controller/clickhouse_keeper.go
+++ controller/clickhouse_keeper.go
@@ -199,8 +199,8 @@ var clickhouseKeeperConfigTemplate = template.Must(template.New("").Parse(`
     <console>1</console>
     <level>information</level>
 </logger>
-<listen_host>::</listen_host>
 <listen_host>0.0.0.0</listen_host>
+<listen_host>::</listen_host>
 <keeper_server>
     <tcp_port>9181</tcp_port>
     <server_id>SERVER_ID</server_id>
